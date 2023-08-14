40f970d6bc4e2d26765c724d7ec47849930f1503
Steven-lei <leizhengwen@126.com>
2023-08-14 22:45:40 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("in app");
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(3000);
+
+console.log("running at 3000");
